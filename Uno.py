f37e660ef5ead65006a79a6500ddde986b511185
Alejandro Cesar Moya <46249970+e1ectriccrasher@users.noreply.github.com>
2022-03-20 13:59:15 -0600

    Changes

--- Uno.py
+++ Uno.py
@@ -3,6 +3,10 @@ print("hello world")
 for x in(range(10)):
   print(x)
 
+
+# x -> is the First name of a person a person
+# y-> is the first name of a second person
+# Saludos -> function wich make x, y greet each other
 def Saludos(x,y):
   print(str(x) + " saluda a " + str(y))
 
