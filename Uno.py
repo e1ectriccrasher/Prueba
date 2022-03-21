c550fef12de8d40355253ed51b0aa0432a01cd2d
Alejandro Cesar Moya <46249970+e1ectriccrasher@users.noreply.github.com>
2022-03-21 11:48:04 -0600

    Update

--- Uno.py
+++ Uno.py
@@ -33,9 +33,11 @@ def Resta(x, y):
   
   # mas cambio por revisar -> ya quedaron los cambios
   
-  print("hola")
+print("hola")
 
-  print("solo son mas pruebas")
+print("solo son mas pruebas")
+
+print("hay mas cosas por arreglar")
 
 
  
